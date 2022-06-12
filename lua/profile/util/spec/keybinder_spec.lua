d98f1413e7717b917900212001fad5b313a93396
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2022-06-12 15:09:40 +0200

    Implemented standard tests to be used for several types of keybindings

--- lua/profile/util/spec/keybinder_spec.lua
+++ lua/profile/util/spec/keybinder_spec.lua
@@ -2,60 +2,157 @@ local mock = require('luassert.mock')
 local match = require('luassert.match')
 -- local stub = require('luassert.stub')
 
-describe("keybinder", function ()
+describe("bind", function ()
   local bind = require('profile.util.keybinder')
-  describe("bind_regular", function ()
-    local snapshot
-    before_each(function ()
-      snapshot = assert:snapshot()
-    end)
-    after_each(function ()
-      snapshot:revert()
-    end)
-    it("Should pass {{ {{'n', 'o', 'v'}, 'w', ':put'} }} to vim.keymap.set", function ()
-      local keymap = mock(vim.keymap, true)
-      bind {{ { {'n', 'o', 'v'}, 'w', ':put' } }}
-      assert.stub(keymap.set).was_called()
-      assert.stub(keymap.set).was_called_with(
-        match.same({'n', 'o', 'v'}), 'w', ':put')
-    end)
-    it("Should pass {{ {'n', 'w', ':put'} }} to vim.keymap.set", function ()
-      local keymap = mock(vim.keymap, true)
-      bind {{ {'n', 'w', ':put' } }}
-      assert.stub(keymap.set).was_called()
-      assert.stub(keymap.set).was_called_with('n', 'w', ':put')
-    end)
+  local snapshot, keymap
+
+  before_each(function ()
+    snapshot = assert:snapshot()
+    keymap = mock(vim.keymap, true)
   end)
-  describe("bind_module", function ()
-    local snapshot
-    before_each(function ()
-      snapshot = assert:snapshot()
+  after_each(function ()
+    snapshot:revert()
+  end)
+
+  local function standard_tests(bind_arg, ...)
+    local expected_binding_count = 0
+    local matchers = {...}
+    for _, t in ipairs(bind_arg) do
+      expected_binding_count = expected_binding_count + #t
+    end
+
+    it("Should bind the correct number of keybindings", function ()
+      bind(bind_arg)
+      assert.stub(keymap.set).was_called(expected_binding_count)
     end)
-    after_each(function ()
-      snapshot:revert()
+
+
+    it("Should call vim.keymap.set with correct arguments", function ()
+      bind(bind_arg)
+      for i, test in ipairs { "mode/modes", "lhs", "rhs", "opts", } do
+        local m = {}; for j = 1, #matchers do m[j] = match._ end
+        if #m >= i then m[i] = matchers[i] end
+        assert.stub(keymap.set, test).was_called_with(unpack(m))
+      end
     end)
-    it("Should bind module functions with vim.keymap.set", function ()
-      local keymap = mock(vim.keymap, true)
-      bind {{
-        module = 'profile.util.spec.testmodule',
-        {'n', 'w', 'dummy_fn' }
-      }}
-      assert.stub(keymap.set).was_called()
-    end)
-    it("Should require the module and create opt.desc", function ()
-      local keymap = mock(vim.keymap, true)
-      bind {{
-        module = 'profile.util.spec.testmodule',
-        {'n', 'w', 'dummy_fn' }
-      }}
-      assert.stub(keymap.set).was_called()
-      assert.stub(keymap.set).was_called_with(
-        match.equals('n'),
-        match.equals('w'),
-        match.equals(require('profile.util.spec.testmodule').dummy_fn),
+  end
+
+  -- Regular bindings
+  describe("{{'n', 'o', 'v'}, 'w', ':put'}", function ()
+    standard_tests(
+      {{ { {'n', 'o', 'v'}, 'w', ':put' } }},
+      match.same({'n', 'o', 'v'}), 'w', ':put')
+  end)
+  describe("{'n', 'w', ':put'}", function ()
+    standard_tests( {{ {'n', 'w', ':put'} }}, 'n', 'w', ':put')
+  end)
+
+  describe("module = 'testmodule',", function ()
+    describe("{'n', 'w', 'dummy_fn'}", function ()
+      standard_tests({{
+          module = 'profile.util.spec.testmodule',
+          {'n', 'w', 'dummy_fn'}
+        }}, 'n', 'w',
+        require('profile.util.spec.testmodule').dummy_fn,
         match.same({
           desc = 'profile.util.spec.testmodule.dummy_fn'
         }))
     end)
+
+    it("Should not overwrite a desc provided in opts", function ()
+      bind {{
+          module = 'profile.util.spec.testmodule',
+          {'n', 'w', 'dummy_fn', {desc = "Dummy Desc"}}
+        }}
+      assert.stub(keymap.set).was_called()
+      assert.stub(keymap.set).was_called_with(
+        'n', 'w',
+        require('profile.util.spec.testmodule').dummy_fn,
+        match.same({
+          desc = 'Dummy Desc'
+        }))
+    end)
+
+    it("Should not overwrite ", function ()
+      bind {{
+          module = 'profile.util.spec.testmodule',
+          {'n', 'w', 'dummy_fn', {desc = "Dummy Desc"}}
+        }}
+      assert.stub(keymap.set).was_called()
+      assert.stub(keymap.set).was_called_with(
+        'n', 'w',
+        require('profile.util.spec.testmodule').dummy_fn,
+        match.same({
+          desc = 'Dummy Desc'
+        }))
+    end)
+
   end)
+
+  describe("tbl = {dummy_tbl, 'dummy_tbl'}", function ()
+    local dummy_tbl = { dummy_fn = function () end }
+    describe("{'n', 'w', 'dummy_fn'}", function ()
+      standard_tests({{
+          tbl = {dummy_tbl, 'dummy_tbl'},
+          {'n', 'w', 'dummy_fn'}
+        }}, 'n', 'w',
+        dummy_tbl.dummy_fn,
+        match.same({
+          desc = 'dummy_tbl.dummy_fn'
+        }))
+    end)
+  end)
+  -- describe("bind tbl", function ()
+  --   local dummy_tbl = { dummy_fn = function () end }
+  --   it("Should bind to functions in a supplied table", function ()
+  --     bind {{
+  --       tbl = { dummy_tbl, 'dummy_tbl', },
+  --       {'n', 'w', 'dummy_fn'}
+  --     }}
+  --     assert.stub(keymap.set).was_called(1)
+  --     assert.stub(keymap.set).was_called_with(
+  --       match.equals('n'),
+  --       match.equals('w'),
+  --       match.equals(require('profile.util.spec.testmodule').dummy_fn),
+  --       match._,
+  --       match.same({
+  --         desc = 'profile.util.spec.testmodule.dummy_fn'
+  --       }))
+  --   end)
+  --   it("Should bind and supply an automatic desc to opts", function ()
+  --     bind {{
+  --       tbl = { dummy_tbl, 'dummy_tbl', },
+  --       {'n', 'w', 'dummy_fn'}
+  --     }}
+  --     assert.stub(keymap.set).was_called(1)
+  --     assert.stub(keymap.set).was_called_with(
+  --       match.equals('n'),
+  --       match.equals('w'),
+  --       match._,
+  --       match.same({ desc = 'dummy_tbl.dummy_fn' }))
+  --   end)
+  -- end)
+  -- describe("bind module", function ()
+  --   it("Should bind module functions with vim.keymap.set", function ()
+  --     bind {{
+  --       module = 'profile.util.spec.testmodule',
+  --       {'n', 'w', 'dummy_fn'}
+  --     }}
+  --     assert.stub(keymap.set).was_called(1)
+  --   end)
+  --   it("Should require the module and create opt.desc", function ()
+  --     bind {{
+  --       module = 'profile.util.spec.testmodule',
+  --       {'n', 'w', 'dummy_fn'}
+  --     }}
+  --     assert.stub(keymap.set).was_called(1)
+  --     assert.stub(keymap.set).was_called_with(
+  --       match.equals('n'),
+  --       match.equals('w'),
+  --       match.equals(require('profile.util.spec.testmodule').dummy_fn),
+  --       match.same({
+  --         desc = 'profile.util.spec.testmodule.dummy_fn'
+  --       }))
+  --   end)
+  -- end)
 end)
