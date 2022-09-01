246c3029e92baa8b46243984d42d1b661366a270
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2022-09-01 11:31:35 +0200

    Added an unversioned file for containing machine local configs

---
+++ lua/profile/machine_local.lua
@@ -0,0 +1,10 @@
+---This file is meant to contain settings that are local to the machine.
+---E.g. absolute paths, etc.
+---
+---It should therefore not be versioned in the git repository
+
+local dap_settings = require'profile.setups.dap'.settings
+
+dap_settings.cppdbg {
+  vscode_cpptools_path = '/home/osksod/tools/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7'
+}
