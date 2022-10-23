dcef606c0d7c86e18f9e1cbfefe61b8dadec6f05
Oskar Söderbärg <osken_o.s@hotmail.com>
2022-10-23 11:38:05 +0200

    Telescope file browser and added tags.lock to ignore

--- init.lua
+++ init.lua
@@ -40,6 +40,7 @@ require('packer').startup(function(use)
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { "nvim-telescope/telescope-file-browser.nvim" }
   use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   -- Add indentation guides even on blank lines
