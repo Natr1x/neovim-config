d4c5d5897505600f66f7815c8bedea470001620a
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2023-04-10 10:55:00 +0200

    Maybe this should just be removed completely

--- lua/profile/machine_local.lua
+++ lua/profile/machine_local.lua
@@ -3,9 +3,9 @@
 ---
 ---It should therefore not be versioned in the git repository
 
-local dap_settings = require'profile.setups.dap'.settings
+--local dap_settings = require'profile.setups.dap'.settings
 
-dap_settings.cppdbg {
-  vscode_cpptools_path = '/home/osksod/tools/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7',
-  vscode_cortex_debug_path = '/home/osksod/tools/vscode-cortex-debug/cortex-debug/dist/debugadapter.js',
-}
+-- dap_settings.cppdbg {
+--   vscode_cpptools_path = '/home/osksod/tools/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+--   vscode_cortex_debug_path = '/home/osksod/tools/vscode-cortex-debug/cortex-debug/dist/debugadapter.js',
+-- }
