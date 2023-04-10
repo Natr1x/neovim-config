3ba9fb232785ff8d43b007c2bd04ef6b76d7cbb9
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2023-04-10 12:03:57 +0200

    Updated keymaps for debugging

--- lua/profile/keymaps/dap.lua
+++ lua/profile/keymaps/dap.lua
@@ -1,26 +1,37 @@
 local bind = require('profile.util.keybinder')
+local wk = require 'which-key'
+
+local function nmap(keys, func, desc)
+  return { 'n', keys, func, { desc = 'DAP: ' .. desc } }
+end
+
+wk.register({
+  ['<LocalLeader>d'] = { name = '+debug' },
+  ['<LocalLeader>ds'] = { name = '+step' },
+  ['<LocalLeader>du'] = { name = '+debugui' },
+})
 
 bind {{
-  {
-    'n', '<LocalLeader>db',  ':DapToggleBreakpoint<cr>',
-    { silent = true, desc = 'Toggle [d]ebug [b]reakpoint' }
+    module = 'dap',
+    nmap('<LocalLeader>db', 'toggle_breakpoint', 'Toggle [d]ebug [b]reakpoint'),
+    nmap('<LocalLeader>dc', 'continue', '[d]ebug [c]ontinue'),
+    nmap('<LocalLeader>dl', 'run_last', '[d]ebug run [l]ast'),
+    nmap('<LocalLeader>dr', 'run_to_cursor', '[d]ebug [r]un to cursor'),
+    nmap('<LocalLeader>dsi',  'step_into', '[d]ebug [s]tep [i]nto'),
+    nmap('<LocalLeader>dso',  'step_over', '[d]ebug [s]tep [o]ver'),
+    nmap('<LocalLeader>dsO',  'step_out', '[d]ebug [s]tep [O]ut'),
   },
   {
-    'n', '<LocalLeader>dc',  ':DapContinue<cr>',
-    { silent = true, desc = '[debug] [c]ontinue' }
+    nmap('<LocalLeader>dB', function ()
+      require('dap').set_breakpoint(vim.fn.input, 'Breakpoint condition: ')
+    end, 'DAP: Set conditional [d]ebug [B]reakpoint'),
   },
   {
-    'n', '<LocalLeader>dsi',  ':DapStepInto<cr>',
-    { silent = true, desc = '[d]ebug [s]tep [i]nto' }
+    module = 'dapui',
+    nmap('<LocalLeader>duc', 'close', '[d]ebug [u]i [c]lose'),
+    nmap('<LocalLeader>duo', 'close', '[d]ebug [u]i [o]pen'),
+    nmap('<LocalLeader>duf', 'close', '[d]ebug [u]i [f]loat element'),
   },
-  {
-    'n', '<LocalLeader>dso',  ':DapStepOver<cr>',
-    { silent = true, desc = '[d]ebug [s]tep [o]ver' }
-  },
-  {
-    'n', '<LocalLeader>dsO',  ':DapStepOut<cr>',
-    { silent = true, desc = '[d]ebug [s]tep [O]ut' }
-  },
-}}
+}
 
 -- @TODO: Setup dap binding for this "dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')"
