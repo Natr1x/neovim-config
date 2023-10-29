8ccb5241f1fff9d77aa44724e65d438faafefd71
Natrix <osken_o.s@hotmail.com>
2023-10-29 09:49:46 +0100

    Updated plugins

--- lua/profile/plugins/ui.lua
+++ lua/profile/plugins/ui.lua
@@ -3,10 +3,8 @@ return {
 
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    }
+    main = "ibl",
+    opts = { }
   },
 
   { -- Set statusbar
