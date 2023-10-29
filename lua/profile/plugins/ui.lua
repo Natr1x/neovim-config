22477b3f99187b11779bed065dd13ce24d6bc597
Natrix <osken_o.s@hotmail.com>
2023-10-29 11:02:56 +0100

    Change to new line character

--- lua/profile/plugins/ui.lua
+++ lua/profile/plugins/ui.lua
@@ -4,7 +4,11 @@ return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
-    opts = { }
+    opts = {
+      indent = {
+        char = '┊',
+      },
+    },
   },
 
   { -- Set statusbar
