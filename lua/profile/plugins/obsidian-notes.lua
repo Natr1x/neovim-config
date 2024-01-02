6058c11faf10b42049ba27bebae7e41b75280e72
Natrix <osken_o.s@hotmail.com>
2024-01-02 11:54:14 +0100

    Configured keybindings for obsidian plugin

--- lua/profile/plugins/obsidian-notes.lua
+++ lua/profile/plugins/obsidian-notes.lua
@@ -2,6 +2,17 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
   lazy = true,
+  cmd = { -- Lazy load on these commands
+    "ObsidianSearch",
+    "ObsidianQuickSwitch",
+    "ObsidianToday",
+    "ObsidianYesterday",
+    "ObsidianTomorrow",
+    "ObsidianNew",
+    "ObsidianTemplate",
+    "ObsidianOpen",
+    "ObsidianWorkspace",
+  },
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
