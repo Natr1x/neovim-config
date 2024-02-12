13d6737a215ef797bb63a38b9318bf62f0987b12
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2024-02-12 09:51:41 +0100

    Only enable obsidian plugin for vault for now since it can be slow with large files

--- lua/profile/plugins/obsidian-notes.lua
+++ lua/profile/plugins/obsidian-notes.lua
@@ -13,14 +13,14 @@ return {
     "ObsidianOpen",
     "ObsidianWorkspace",
   },
-  ft = "markdown",
+  -- ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre path/to/my-vault/**.md",
+    "BufNewFile path/to/my-vault/**.md",
+  },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
