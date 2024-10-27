f5b9016e882d02cb5012f7d17562da8907ddfd65
Natrix <osken_o.s@hotmail.com>
2024-10-27 11:15:41 +0100

    This should not be needed since I generally use another ls for cs

--- lua/profile/plugins/lsp/init.lua
+++ lua/profile/plugins/lsp/init.lua
@@ -6,7 +6,7 @@ local servers = {
       '--query-driver="/usr/bin/gcc,/usr/bin/g++,/usr/bin/arm-none-eabi-gcc,/usr/bin/arm-none-eabi-g*,/opt/st/stm32cubeide_1.13.2/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.11.3.rel1.linux64_1.1.1.202309131626/tools/bin/arm-none-eabi-gcc,/usr/bin/arm-none-eabi-g*,/opt/st/stm32cubeide_1.13.2/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.11.3.rel1.linux64_1.1.1.202309131626/tools/bin/arm-none-eabi-g++,/usr/bin/avr-gcc"',
     },
   },
-  omnisharp = {},
+  -- omnisharp = {},
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
