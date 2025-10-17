2892af4ae999a91ea9c811b71e5ad2ea75c9d879
Natrix <osken_o.s@hotmail.com>
2025-10-17 15:17:13 +0200

    Why have I not fixed this earlier?

--- lua/profile/keymaps/completion.lua
+++ lua/profile/keymaps/completion.lua
@@ -13,18 +13,14 @@ return cmp.mapping.preset.insert({
     select = true,
   },
   ['<M-n>'] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
+    if luasnip.expand_or_jumpable() then
       luasnip.expand_or_jump()
     else
       fallback()
     end
   end, { 'i', 's' }),
   ['<M-p>'] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
+    if luasnip.jumpable(-1) then
       luasnip.jump(-1)
     else
       fallback()
