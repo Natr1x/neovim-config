65a3678a36121693a56ee1cfc8dbe4a1ff2d58fc
Natrix <osken_o.s@hotmail.com>
2026-02-03 17:21:16 +0100

    Cleaned up the lsp configs a bit

--- lua/profile/plugins/lsp/init.lua
+++ lua/profile/plugins/lsp/init.lua
@@ -1,5 +1,4 @@
 
--- Servers here will be automatically installed and setup with these settings
 local servers = {
   clangd = {
     arguments = {
@@ -13,64 +12,24 @@ local servers = {
       telemetry = { enable = false },
     },
   },
-
 }
 
+vim.lsp.config['rust-analyzer'] = {
+  completion = {
+    postfix = {
+      enable = false,
+    },
+    autoimport = {
+      enable = false,
+    },
+    callable = {
+      snippets = "none",
+    },
+  },
+}
+
+
 require 'profile.keymaps.lsp'
-
---- Generates a default setup table to pass to servers setup function
-local function default_setup_handler(server_name)
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-  local lsp_bindings = require 'profile.keymaps.lsp'
-  local set_lsp_keybindings = lsp_bindings.get_on_attach_fn(server_name)
-
-  return {
-    capabilities = capabilities,
-    on_attach = function (client, buffnr)
-      if client.server_capabilities.signatureHelpProvider then
-        require('lsp-overloads').setup(client, {})
-      end
-      set_lsp_keybindings(client, buffnr)
-    end,
-    settings = servers[server_name],
-  }
-end
-
-local function mason_lspconfig_setup(_, opts)
-  local mason_lspconfig = require 'mason-lspconfig'
-  mason_lspconfig.setup(opts)
-  mason_lspconfig.setup_handlers {
-    function(server_name)
-      local setup = default_setup_handler(server_name)
-      require('lspconfig')[server_name].setup(setup)
-    end,
-
-    ['omnisharp'] = function ()
-      local setup = default_setup_handler('omnisharp')
-      setup.handles = {
-        ['textDocument/definition'] = require('omnisharp_extended').handler
-      }
-    end,
-
-    ['rust_analyzer'] = function ()
-
-      -- local lsp_bindings = require 'profile.keymaps.lsp'
-      -- local set_lsp_keybindings = lsp_bindings.get_on_attach_fn('rust_analyzer')
-      -- require("rust-tools").setup {
-      --   server = {
-      --     on_attach = set_lsp_keybindings,
-      --   },
-      --   dap = {
-      --     adapter = require('mason-nvim-dap.mappings.adapters').codelldb
-      --   }
-      -- }
-
-    end
-
-  }
-end
 
 vim.g.rustaceanvim = function ()
   local lsp_bindings = require 'profile.keymaps.lsp'
@@ -82,26 +41,7 @@ vim.g.rustaceanvim = function ()
     -- LSP configuration
     server = {
       on_attach = set_lsp_keybindings,
-      default_settings = {
-        -- rust-analyzer language server configuration
-        ['rust-analyzer'] = {
-          completion = {
-            postfix = {
-              enable = false,
-            },
-            autoimport = {
-              enable = false,
-            },
-            callable = {
-              snippets = "none",
-            },
-          },
-        },
-      },
     },
-    -- DAP configuration
-    -- dap = {
-    -- },
   }
 end
 
@@ -112,16 +52,6 @@ return {
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
       { 'williamboman/mason.nvim', opts = {} },
-      -- {
-      --   'williamboman/mason-lspconfig.nvim',
-      --   opts = { ensure_installed = vim.tbl_keys(servers), },
-      --   config = mason_lspconfig_setup,
-      --
-      --   dependencies = {
-      --     -- Additional lua configuration, makes nvim stuff amazing!
-      --     { 'folke/neodev.nvim', opts = {} },
-      --   },
-      -- },
 
       -- Useful status updates for LSP
       {
